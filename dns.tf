54a93f445783525bf75b91ac18e59522aa9b0aee
Yeongtaek Hong <mozodev@cckorea.org>
2014-11-30 23:27:35 +0900

    Renamed mozo.kr to dns.tf and using variable

--- dns.tf
+++ dns.tf
@@ -1,12 +1,12 @@
 # Create a new domain record
 resource "digitalocean_domain" "default" {
-  name = "mozo.kr"
+  name = "${var.domain}"
   ip_address = "${digitalocean_droplet.web.ipv4_address}"
 }
 
 # Add a record to the domain
 resource "digitalocean_record" "www" {
-  domain = "${digitalocean_domain.web.name}"
+  domain = "${digitalocean_domain.default.name}"
   type = "CNAME"
   name = "www"
   value = "@"
